87c4f27c1323449577ad45bb8874c61bee73cca3
a.m.student <ninjaalec12@gmail.com>
2021-01-20 08:33:08 -0600

    Initial Commit

---
+++ yoit/yoitApp.swift
@@ -0,0 +1,17 @@
+//
+//  yoitApp.swift
+//  yoit
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct yoitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
